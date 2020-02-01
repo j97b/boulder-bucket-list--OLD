a35889ddc559684af485129abba5b95b5557d439
j97b <jamesbarron97@gmail.com>
2020-02-01 21:28:06 +0000

    user add updated, login form added

--- src/main/java/com/bae/persistence/domain/User.java
+++ src/main/java/com/bae/persistence/domain/User.java
@@ -3,14 +3,7 @@ package com.bae.persistence.domain;
 import java.util.Objects;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "user")
--- src/main/resources/static/javaScript/index.js
+++ src/main/resources/static/javaScript/index.js
@@ -1,43 +1,37 @@
-let urlPre="/bbl";
-
-function makeUsersTable() {
-    axios.get(urlPre + "/userApp/user")
-        .then(response => {
-            response.data.forEach(user => {
-                let table = document.getElementById('names');
-                let tr = document.createElement('tr');
-                let td = document.createElement('td');
-                tr.setAttribute('id',user.id);
-                tr.setAttribute('class',"nameRows");
-                tr.addEventListener('click', () => {
-                    sessionStorage.setItem("userID",user.id);
-                    window.location = "html/bucketList.html";
-                });
-                let name = user.name.toString();
-                td.innerText = name.charAt(0).toUpperCase() + name.substring(1).toLowerCase();
-                table.appendChild(tr);
-                tr.appendChild(td);
-            });
-        }).catch(err => console.error(err));
-}
+let urlPre="";
+let valid = true;
 
 function addUser() {
-    let name = document.getElementById("userName");
-    if ( name.value === "" || name.value.length > 20) {
-        document.getElementById("invalidNameErrorMessage").setAttribute("style","");
+    let name = document.getElementById("userName").value;
+    let password = document.getElementById("userPassword").value;
+    let passwordConfirm = document.getElementById("userPasswordConfirm").value;
+    if (name === "" || password === "") {
+        document.getElementById("noEntryErrorMessage").setAttribute("style","");
+    } else if (name.length > 20) {
+        document.getElementById("nameTooLongErrorMessage").setAttribute("style","");
+    } else if (password !== passwordConfirm) {
+        document.getElementById("differentPasswordsErrorMessage").setAttribute("style","");
     } else {
-        let user = {
-            "name": name.value,
-            "boulders": []
-        }
-        axios.post(urlPre + "/userApp/user", user).then( () => {
-            window.location.reload();
-        });
+            let user = {
+                "username": name,
+                "hashedPassword": password,
+                "boulders": []
+            }
+            axios.post(urlPre + "/userApp/user", user).then(() => {
+                $('#addUserForm').modal('hide')
+            });
     }
 }
 
 function resetAddModal() {
     let name = document.getElementById("userName");
-    document.getElementById("invalidNameErrorMessage").setAttribute("style", "display: none");
+    let password = document.getElementById("userPassword");
+    let passwordConfirm = document.getElementById("userPasswordConfirm");
+    document.getElementById("noEntryErrorMessage").setAttribute("style", "display: none");
+    document.getElementById("nameTooLongErrorMessage").setAttribute("style","display: none");
+    document.getElementById("nonUniqueNameErrorMessage").setAttribute("style","display: none");
+    document.getElementById("differentPasswordsErrorMessage").setAttribute("style","display: none");
     name.value = "";
+    password.value="";
+    passwordConfirm.value="";
 }

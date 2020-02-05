9a391ff4070589066def76bc4681fc529b667f9e
j97b <jamesbarron97@gmail.com>
2020-02-05 15:41:34 +0000

    selenium tests added back in

--- src/test/java/com/bae/tests/selenium/pages/IndexPage.java
+++ src/test/java/com/bae/tests/selenium/pages/IndexPage.java
@@ -1,33 +1,54 @@
-//package com.bae.tests.selenium.pages;
-//
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.support.FindBy;
-//
-//public class IndexPage {
-//    @FindBy(xpath = "/html/body/div/button")
-//    private WebElement addUserButton;
-//
-//    @FindBy(xpath = "/html/body/div[4]/table/tr[1]/td")
-//    private WebElement usersTable;
-//
-//    @FindBy(id = "userName")
-//    private WebElement nameInput;
-//
-//    @FindBy(xpath = "/html/body/div[5]/div/div/div[3]/button[2]")
-//    private WebElement submitButton;
-//
-//    public void addUser(String user) {
-//        this.addUserButton.click();
-//        this.nameInput.sendKeys(user);
-//        this.submitButton.click();
-//    }
-//
-//    public boolean checkUser(String user) {
-//        return usersTable.getText().contains(user);
-//    }
-//
-//    public void login() {
-//        this.usersTable.click();
-//    }
-//
-//}
+package com.bae.tests.selenium.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.concurrent.TimeUnit;
+
+public class IndexPage {
+    @FindBy(xpath = "/html/body/div/button")
+    private WebElement addUserButton;
+
+    @FindBy(id="userName")
+    private WebElement setUsername;
+
+    @FindBy(id="userPassword")
+    private WebElement setPassword;
+
+    @FindBy(id="userPasswordConfirm")
+    private WebElement confirmPassword;
+
+    @FindBy(xpath = "/html/body/div[6]/div/div/div[3]/button[2]")
+    private WebElement submitButton;
+
+    @FindBy(id="usernameInput")
+    private WebElement inputUsername;
+
+    @FindBy(id="passwordInput")
+    private WebElement inputPassword;
+
+    @FindBy(xpath = "/html/body/div[5]/button")
+    private WebElement signInButton;
+
+    public void addUser(String username, String password) throws InterruptedException {
+        this.addUserButton.click();
+        Thread.sleep(500);
+        this.setUsername.sendKeys(username);
+        Thread.sleep(500);
+        this.setPassword.sendKeys(password);
+        Thread.sleep(500);
+        this.confirmPassword.sendKeys(password);
+        Thread.sleep(500);
+        this.submitButton.click();
+        Thread.sleep(500);
+    }
+
+    public void login(String username, String password) throws InterruptedException {
+        this.inputUsername.sendKeys(username);
+        Thread.sleep(500);
+        this.inputPassword.sendKeys(password);
+        Thread.sleep(500);
+        this.signInButton.click();
+    }
+
+}
